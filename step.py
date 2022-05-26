e2fd6b956dd82ebc05d8de3c36e115a1437f8010
Aravindh <59680100+aravindh698@users.noreply.github.com>
2022-05-26 18:20:55 +0530

    Update step.py

--- step.py
+++ step.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
-from app2 import app
+from app2 import app as application
 if __name__  == '__main__':
-   app.run(debug=True)
+   port = int(os.environ.get('PORT', 5000))
+   application.run(host='0.0.0.0', port=port)
