67a2bf3a969f8684ef23098812f9663e2e15d8ab
Aravindh <59680100+aravindh698@users.noreply.github.com>
2022-05-31 11:58:01 +0530

    Delete step.py

--- step.py
+++
@@ -1,6 +0,0 @@
-import os
-from flask import Flask
-from app2 import app as application
-if __name__  == '__main__':
-   port = int(os.environ.get('PORT', 5000))
-   application.run(host='0.0.0.0', port=port)
